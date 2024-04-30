36b93042d8ea5489286ea76197518e8349510eb7
Bohdan Savin <bogdan07112009@gmail.com>
2024-04-30 21:05:23 +0300

    working posts

--- app/app.py
+++ app/app.py
@@ -8,26 +8,30 @@ app = Flask(__name__)
 
 
 with Session.begin() as session:
-    post1 = Post(title="Test", created=datetime.now(), content="MOCK DATA MOCK DATA MOCK DATA")
+    post1 = Post(title="Test id 1", created=datetime.now(), content="MOCK DATA MOCK DATA MOCK DATA")
     session.add(post1)
-
+    post2 = Post(title="Test id 2", created=datetime.now(), content="MOCK DATA MOCK DATA MOCK DATA")
+    session.add(post2)
 
 @app.get("/")
 def index():
+    context = dict()
     with Session.begin() as session:
-        posts = session.scalars(select(Post)).all()  
-    return render_template("index.html", posts=posts)
+        context["posts"] = session.scalars(select(Post)).all()
+        return render_template("index.html", **context)
 
 
 @app.get("/<int:post_id>")
 def post(post_id):
+    context = dict()
     with Session.begin() as session:
-        post = session.scalars(select(Post).where(Post.id == post_id)).all()
         
-    if post is None:
-        post = abort(404)
+        context["post"] = session.scalars(select(Post).where(Post.id == post_id)).one()
         
-    return render_template("post.html", post=post)
+        if context["post"] is None:
+            context["post"] = abort(404)
+        
+        return render_template("post.html", **context)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
