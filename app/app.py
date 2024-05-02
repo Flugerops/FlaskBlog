32fd4fd8f0e60bf6864f0fb1a71a77b1959390f8
Bohdan Savin <bogdan07112009@gmail.com>
2024-05-02 21:36:59 +0300

    delete and edit working

--- app/app.py
+++ app/app.py
@@ -1,19 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from db import Session, Post
 from sqlalchemy import select
 from werkzeug.exceptions import abort
 from datetime import datetime
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
-
-
-with Session.begin() as session:
-    post1 = Post(title="Test id 1", created=datetime.now(), content="MOCK DATA MOCK DATA MOCK DATA")
-    session.add(post1)
-    post2 = Post(title="Test id 2", created=datetime.now(), content="MOCK DATA MOCK DATA MOCK DATA")
-    session.add(post2)
-    post2 = Post(title="My Walk", created=datetime.now(), content="Now i having the best walk in my life!")
-    session.add(post2)
+app.config["SECRET_KEY"] = f"{getenv('SECRETKEY')}"
 
 @app.get("/")
 def index():
@@ -23,17 +20,68 @@ def index():
         return render_template("index.html", **context)
 
 
+
+
 @app.get("/post/<int:post_id>")
 def _id(post_id):
     context = dict()
     with Session.begin() as session:
-        
+
         context["post"] = session.scalars(select(Post).where(Post.id == post_id)).one()
-        
+
         if context["post"] is None:
             context["post"] = abort(404)
-        
+
         return render_template("post.html", **context)
+
+
+@app.get("/create")
+def create_get():
+    return render_template('create.html')
+
+@app.post("/create")
+def create_post():
+    title = request.form.get("title")
+    content = request.form.get("content")
+    if not title:
+        flash("Title is required!!")
+    else:
+        with Session.begin() as session:
+            post1 = Post(title=title, created=datetime.now(), content=content)
+            session.add(post1)
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+
+@app.get("/edit/<int:edit_id>")
+def edit(edit_id):
+    context = dict()
+    with Session.begin() as session:
+        context["post"] = session.scalars(select(Post).where(Post.id == edit_id)).one()
+    
+        return render_template('edit.html', **context)
+
+@app.post("/edit/<int:edit_id>")
+def edit_post(edit_id):
+    title = request.form.get("title")
+    content = request.form.get("content")
+    with Session.begin() as session:
+        post = session.scalars(select(Post).where(Post.id == edit_id)).one()
+        post.title = title
+        post.content = content
+    flash("Post updated successfully")
+    return redirect(url_for('index'))
+
+@app.get("/edit/delete/<int:edit_id>")
+def delete_post(edit_id):
+    with Session.begin() as session:
+        post = session.scalars(select(Post).where(Post.id == edit_id)).one()
+        if post is None:
+            abort(404)
+        session.delete(post)
+    flash("Post deleted successfully!")
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
